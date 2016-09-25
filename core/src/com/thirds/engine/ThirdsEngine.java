a823491ca5fb652b1d151f17e0e782f52a5a6c3e
ScrollFusionAdmin <scrollfusion@gmail.com>
2016-09-25 20:54:15 +0100

    Initial commit

---
+++ core/src/com/thirds/engine/ThirdsEngine.java
@@ -0,0 +1,18 @@
+package com.thirds.engine;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class ThirdsEngine extends ApplicationAdapter {
+	
+	@Override
+	public void create() {
+	}
+
+	@Override
+	public void render() {
+	}
+	
+	@Override
+	public void dispose() {
+	}
+}
