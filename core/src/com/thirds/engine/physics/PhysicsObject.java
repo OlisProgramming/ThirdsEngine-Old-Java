a56de0e6ac7c29881ba3715ec75adb5e4af0ebdd
ScrollFusionAdmin <scrollfusion@gmail.com>
2016-09-30 18:20:29 +0100

    Add PhysicsObject

---
+++ core/src/com/thirds/engine/physics/PhysicsObject.java
@@ -0,0 +1,42 @@
+package com.thirds.engine.physics;
+
+import com.badlogic.gdx.math.Vector3;
+import com.thirds.engine.ThirdsEngine;
+
+public class PhysicsObject {
+
+	public static enum PhysicsSimulationType {
+		STATIC,
+		DYNAMIC
+	}
+	
+	private PhysicsSimulationType simType;
+	private Vector3 pos;
+	private Vector3 velocity;
+	
+	public PhysicsObject(Vector3 pos) {
+		this(pos, PhysicsSimulationType.DYNAMIC);
+	}
+	
+	public PhysicsObject(Vector3 pos, PhysicsSimulationType simType) {
+		this.pos = pos;
+		this.simType = simType;
+		velocity = new Vector3(1f, 2f, 3f);
+	}
+
+	public void simulate() {
+		if (simType == PhysicsSimulationType.STATIC) {
+			// Do nothing for static objects
+		} else if (simType == PhysicsSimulationType.DYNAMIC) {
+			pos.add(velocity.scl(ThirdsEngine.SECONDS_PER_TICK));
+		}
+	}
+	
+	public Vector3 getPos() {
+		return pos;
+	}
+
+	public Vector3 getVelocity() {
+		return velocity;
+	}
+}
